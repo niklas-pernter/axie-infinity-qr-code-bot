ccc683fd4fac079134ab3f108d185bbe8068cbe9
Niklas Pernter <63420477+niklas-pernter@users.noreply.github.com>
2021-08-11 15:50:31 +0200

    Create SecretStorage.py

---
+++ SecretStorage.py
@@ -0,0 +1,5 @@
+ScholarsDict = {
+    # 'DiscordID' : ['DiscordName', 'RoninWalletAddress', 'RoninPrivateKey']
+    '000000000000000000' : ['name', '0x{metamask/ronin address}','{metamask/ronin private key}']
+# Put Your Discord Bot Token Here
+DiscordBotToken = 'discord bot token'
